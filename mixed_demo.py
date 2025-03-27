f5a5e96cc7848239d30fdb6d62d8191698914f18
Jonathan Helmus <jjhelmus@gmail.com>
2025-03-27 15:15:36 -0500

    wheel demo

---
+++ mixed_demo.py
@@ -0,0 +1,9 @@
+import spam
+import eggs
+
+
+print("spam doit(2, 3):", spam.doit(2, 3))
+print("spam libfoo version:", spam.get_libfoo_version())
+
+print("eggs doit(2, 3):", eggs.doit(2, 3))
+print("eggs libfoo version:", eggs.get_libfoo_version())
---
+++ whl_demo.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+set -ex
+if [ ! -d ./env ]; then
+    conda create --prefix ./env python=3.12 pip python-build setuptools --yes
+fi
+
+cd spam
+    cc -c -fPIC foo.c
+    cc -shared -o libfoo.so foo.o
+    rm foo.o
+
+    rm -rf build
+    rm -rf dist
+    rm -rf fixed_wheels
+    conda run --prefix ../env python -m build --wheel .
+    conda run --prefix ../env delocate-wheel -w fixed_wheels -v ./dist/*.whl
+    conda run --prefix ../env delocate-listdeps --all fixed_wheels/*.whl
+cd ..
+
+cd eggs
+    cc -c -fPIC foo.c
+    cc -shared -o libfoo.so foo.o
+    rm foo.o
+
+    rm -rf build
+    rm -rf dist
+    rm -rf fixed_wheels
+    conda run --prefix ../env python -m build --wheel .
+    conda run --prefix ../env delocate-wheel -w fixed_wheels -v ./dist/*.whl
+    conda run --prefix ../env delocate-listdeps --all fixed_wheels/*.whl
+cd ..
+
+conda create --prefix ./test_env python=3.12 pip --yes
+conda run --prefix ./test_env python -m pip install ./spam/fixed_wheels/*.whl
+conda run --prefix ./test_env python -m pip install ./eggs/fixed_wheels/*.whl
+conda run --prefix ./test_env python mixed_demo.py
